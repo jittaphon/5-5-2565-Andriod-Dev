9c98d3f9479058c19e8d093dd87848de71dbdb38
artjitt9611 <artjitt2512@gmail.com>
2022-05-14 21:54:40 +0700

    send data to anthor page

--- secondapplink/lib/pages/detail.dart
+++ secondapplink/lib/pages/detail.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+
 class DetailPage extends StatefulWidget {
-  const DetailPage({ Key? key }) : super(key: key);
+  const DetailPage({
+    Key? key,
+    required this.detail,
+    required this.title,
+    required this.subtitle,
+    required this.image,
+  }) : super(key: key);
+
+  final String detail;
+  final String subtitle;
+  final String title;
+  final String image;
 
   @override
   State<DetailPage> createState() => _DetailPageState();
@@ -10,12 +22,17 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Details"),),
+      appBar: AppBar(
+        title: Text("Details"),
+      ),
       body: ListView(
         children: [
-          Text("Details Explanation")
+          Text(widget.title),
+          Text(widget.subtitle),
+          Text(widget.detail),
+          Image.network(widget.image),
         ],
       ),
-    ) ;
+    );
   }
 }
--- secondapplink/lib/pages/home.dart
+++ secondapplink/lib/pages/home.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'detail.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -15,55 +15,79 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(title: Text("Computer Knowledge")),
       body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: FutureBuilder(
-          builder: (context, snapshot){
-            var data = json.decode(snapshot.data.toString());
-            return ListView.builder(itemBuilder:(BuildContext context , int index){
-              return MyBox(data[index]['title'],data[index]['subtitle'],data[index]['image_url'],data[index]['detail']);
-                   
-
+          padding: const EdgeInsets.all(20),
+          child: FutureBuilder(
+            builder: (context, snapshot) {
+              var data = json.decode(snapshot.data.toString());
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return MyBox(data[index]['title'], data[index]['subtitle'],
+                      data[index]['image_url'], data[index]['detail']);
+                },
+                itemCount: data.length,
+              );
             },
-            itemCount: data.length,);
-          },
-          future: DefaultAssetBundle.of(context).loadString('assets/data.json'),) // ใส่ future เพื่อให้รอ data ออกมาให้หมดก่อนนะ
-      ),
+            future:
+                DefaultAssetBundle.of(context).loadString('assets/data.json'),
+          ) // ใส่ future เพื่อให้รอ data ออกมาให้หมดก่อนนะ
+          ),
     );
-      
-   
   }
-  
-Widget MyBox(String title,String subtitle,String image, String detail){ // สร้าง widget เองนักเลงพอ // custom widget // สร้าง widget เป็น function
-  return Container(
-    margin: EdgeInsets.only(top: 20),
-    padding: EdgeInsets.all(20),
-    height: 200,
-    decoration: BoxDecoration(
-      color: Colors.blue[200],
-      borderRadius: BorderRadius.circular(20),
-      image:DecorationImage(
-      image:NetworkImage(image),
-      fit:BoxFit.cover,
-      colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5),BlendMode.darken)
-      )
-  
-    ),
-    child: Column
-    (
-      mainAxisAlignment:MainAxisAlignment.start,
-      crossAxisAlignment:CrossAxisAlignment.start,
-      children: [
-      Text(title, style: TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.bold),),
-      Text(subtitle, style: TextStyle(fontSize: 25,color: Colors.white,fontWeight: FontWeight.bold),),
-      Text(detail, style: TextStyle(fontSize: 10,color: Colors.white,fontWeight: FontWeight.bold),),
-       TextButton(
-              onPressed:() {
-                print("next page >>");
-                Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage()));
-              }, 
-            child: Text("readmore")),
-    ]),
-  );
-}
 
+  Widget MyBox(String title, String subtitle, String image, String detail) {
+    // สร้าง widget เองนักเลงพอ // custom widget // สร้าง widget เป็น function
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.all(20),
+      height: 200,
+      decoration: BoxDecoration(
+          color: Colors.blue[200],
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+              image: NetworkImage(image),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.5), BlendMode.darken))),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              subtitle,
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              detail,
+              style: TextStyle(
+                  fontSize: 10,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => DetailPage(
+                                title: title,
+                                subtitle: subtitle,
+                                detail: detail,
+                                image: image,
+                              )));
+                  print("next page >>");
+                },
+                child: Text("readmore")),
+          ]),
+    );
+  }
 }
